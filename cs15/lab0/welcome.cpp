0c886abd6b156d4d1002e2b78fce16778be5aac3
alee22 <alexander.jae.lee@gmail.com>
2024-05-24 22:40:36 -0600

    Commit 1

---
+++ cs15/lab0/welcome.cpp
@@ -0,0 +1,19 @@
+/* 
+ * Name: Alexander Lee
+ * Date: 5/22/2024
+ * Lab 0
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Welcome to CS 15!" << endl;
+
+  // Add your new message here.
+  cout << "Alexander Lee's first program." << endl;
+  
+  return 0;
+}
